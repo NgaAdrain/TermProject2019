dbd0e388c3930fd020efe0422cd420aeadfdf99a
Banghyungjin <Banghyungjin@github.com>
2019-04-25 00:26:23 +0900

    2차수정_방형진
    -bgm추가
    -setting 메뉴 bgm 스위치 수정필요

--- app/src/main/java/com/example/game_001/GameStartActivity.java
+++ app/src/main/java/com/example/game_001/GameStartActivity.java
@@ -1,19 +1,24 @@
 package com.example.game_001;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class GameStartActivity extends AppCompatActivity {
+
+
     public Button Settingbtn;
     public Button Gamemodebtn;
     public static final int gameSetting = 2001;
     public static final int gameMode = 1002;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_game_start);
         Settingbtn = (Button) findViewById(R.id.SettingButton);
         Settingbtn.setOnClickListener(new View.OnClickListener() {
@@ -32,4 +37,6 @@ public class GameStartActivity extends AppCompatActivity {
             }
         });
     }
+
+
 }
--- app/src/main/java/com/example/game_001/MainActivity.java
+++ app/src/main/java/com/example/game_001/MainActivity.java
@@ -5,18 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-//import android.media.MediaPlayer;
+import android.media.MediaPlayer;
 
 public class MainActivity extends AppCompatActivity {
     final static int ACT_EDIT = 0;
-    //private static MediaPlayer mp;
+
     public Button startbtn;
     public static final int gameStart = 1001;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Intent intent = new Intent(getApplicationContext(),BackgroundMusic.class);
+        startService(intent);
 
         startbtn = (Button) findViewById(R.id.startbutton);
         startbtn.setOnClickListener(new View.OnClickListener() {
@@ -27,4 +28,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
+
+
 }
